2c3301434986d5c7ac716d9ed83df55307c716c1
unknown <szejku141@gmail.com>
2023-03-20 18:21:01 +0100

    Init Spring Boot

---
+++ social-network/src/test/java/spring/summer/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.summer.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
