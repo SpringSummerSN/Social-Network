d3d2dc030f917ddc29d25ddd8ac041b10111a1d6
unknown <szejku141@gmail.com>
2023-03-23 11:43:43 +0100

    Dodanie do main'a klay aplikacyjnej exclude = {DataSourceAutoConfiguration.class

--- social-network/src/main/java/spring/summer/socialnetwork/SocialNetworkApplication.java
+++ social-network/src/main/java/spring/summer/socialnetwork/SocialNetworkApplication.java
@@ -2,12 +2,14 @@ package spring.summer.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SocialNetworkApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SocialNetworkApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
