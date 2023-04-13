f1d159f7ee48078d6b56579b9b5891e63af956a1
Damian Tomczyszyn <damitom@o2.pl>
2023-04-13 14:20:58 +0200

    Dodanie adnotacji

--- social-network/src/main/java/spring/summer/socialnetwork/repositories/UserRepository.java
+++ social-network/src/main/java/spring/summer/socialnetwork/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package spring.summer.socialnetwork.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import spring.summer.socialnetwork.models.User;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     public List<User> findAll();
 }
