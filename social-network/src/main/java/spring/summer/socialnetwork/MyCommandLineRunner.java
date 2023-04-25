bb991898bc28d0ef28ff46d3d9e13c78d09a7af6
Iaura-w <laura.wo19@gmail.com>
2023-04-25 23:09:02 +0200

    added mockups

--- social-network/src/main/java/spring/summer/socialnetwork/MyCommandLineRunner.java
+++ social-network/src/main/java/spring/summer/socialnetwork/MyCommandLineRunner.java
@@ -6,16 +6,20 @@ import org.springframework.stereotype.Component;
 import spring.summer.socialnetwork.migrations.Migration;
 
 @Component
-public class MyCommandLineRunner  implements CommandLineRunner {
+public class MyCommandLineRunner implements CommandLineRunner {
 
     private final Migration migration;
+    private final Mockup mockup;
+
     @Autowired
-    public MyCommandLineRunner(Migration migration) {
+    public MyCommandLineRunner(Migration migration, Mockup mockup) {
         this.migration = migration;
+        this.mockup = mockup;
     }
 
     @Override
     public void run(String... args) throws Exception {
         migration.initialize_db();
+        mockup.mockData();
     }
 }
