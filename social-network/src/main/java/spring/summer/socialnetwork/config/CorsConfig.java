78dfedcbc7c74434ee5930f501d27b6a8016fe1c
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-05-12 23:44:36 +0200

    updated cors config to allow user credentials

--- social-network/src/main/java/spring/summer/socialnetwork/config/CorsConfig.java
+++ social-network/src/main/java/spring/summer/socialnetwork/config/CorsConfig.java
@@ -13,9 +13,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200");
+
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
