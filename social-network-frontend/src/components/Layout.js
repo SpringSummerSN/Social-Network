7913d4e86cf340d293edcb28dccadef82e62966e
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-06-06 22:37:33 +0200

    fixed layout

--- social-network-frontend/src/components/Layout.js
+++ social-network-frontend/src/components/Layout.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Layout = () => {
   const { auth } = useAuth();
+
   return (
     <main className="App">
       {auth?.token && <Navbar />}
-      <div className="container mx-auto max-w-7xl my-3 px-4 sm:px-6 lg:px-8 break-all">
+
+      <div className="layout-container">
         <Outlet />
       </div>
+
+      {auth?.token && <Footer />}
     </main>
   );
 };
