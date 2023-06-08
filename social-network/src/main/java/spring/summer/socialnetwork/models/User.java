abd216188d381340ff8891caeae4ed9ff6bfd07b
Tomasz <korniszuk.tomasz@gmail.com>
2023-06-08 11:59:37 +0200

    add log

--- social-network/src/main/java/spring/summer/socialnetwork/models/User.java
+++ social-network/src/main/java/spring/summer/socialnetwork/models/User.java
@@ -21,7 +21,6 @@ import org.springframework.hateoas.RepresentationModel;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import spring.summer.socialnetwork.validator.ValidPassword;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -38,12 +37,7 @@ public class User extends RepresentationModel<User> implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Email(message = "Email is not valid", regexp =
-            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b" +
-                    "\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:" +
-                    "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
-                    "\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:" +
-                    "[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @Email(message = "Email is not valid")
     @NotEmpty(message = "Email cannot be empty")
     @Column(nullable = false)
     private String email;
